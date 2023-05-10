ba8ede693471c74daf23a6f353c9e4faa80f34b8
BaeYeonDoo <isabelle2k@naver.com>
2023-05-10 22:50:13 +0900

    Update JadenCase 문자열 만들기.py

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -9,3 +9,5 @@ def solution(s):
             new=''
     answer.append(new.capitalize())
     return ' '.join(answer)
+    #answer 리스트 사이에 공백을 넣어 return하기 때문에
+    #공백이 연속으로 또는 앞뒤에 나왔을 때 빈칸을 answer에 넣어주어 공백을 구현할 수 있습니다.
