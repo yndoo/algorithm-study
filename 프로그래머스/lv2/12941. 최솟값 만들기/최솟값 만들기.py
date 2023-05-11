4be42fcf20c65298bd9aa85c50d9eeadb2f51cc1
BaeYeonDoo <isabelle2k@naver.com>
2023-05-11 22:53:28 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.27 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,7 +1,7 @@
 def solution(A,B):
     answer = 0
-    A.sort()
-    B.sort(reverse=True)
-    for i in range(len(A)):
-        answer+=A[i]*B[i]
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
     return answer
