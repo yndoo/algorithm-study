d96f2b6c3d422dddfa54ddf47dfcb264832128b8
BaeYeonDoo <isabelle2k@naver.com>
2023-05-22 14:38:10 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.16 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,11 +1,6 @@
 def solution(n, words):
-    answer = []
     for i,word in enumerate(words):
         if i==0: continue
         if word[0] != words[i-1][-1] or word in words[:i]:
-            print(i, word)
-            answer.append(n) if (i+1)%n==0 else answer.append((i+1)%n)
-            answer.append((i+1)//n) if (i+1)%n==0 else answer.append((i+1)//n+1)
-            return answer
-                    
+            return [i%n+1, i//n+1]                   
     else: return [0,0]
