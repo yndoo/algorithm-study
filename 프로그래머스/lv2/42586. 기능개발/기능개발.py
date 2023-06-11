475250118385246dfb0f405be5121688f221183e
BaeYeonDoo <isabelle2k@naver.com>
2023-06-11 21:00:04 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,5 +1,23 @@
 def solution(progresses, speeds):
     answer = []
+    time = 0
+    cnt = 0
+    while len(progresses):
+        if progresses[0] + time * speeds[0]>=100: 
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt+=1
+        else:
+            if cnt>0:
+                answer.append(cnt)
+                cnt=0
+            time+=1
+    answer.append(cnt)
+    return answer
+
+'''
+def solution(progresses, speeds):
+    answer = []
     days = []
     for i,prg in enumerate(progresses):
         if (100-prg)%speeds[i]==0:
@@ -17,3 +35,4 @@ def solution(progresses, speeds):
             cnt+=1
     answer.append(cnt)
     return answer
+'''
