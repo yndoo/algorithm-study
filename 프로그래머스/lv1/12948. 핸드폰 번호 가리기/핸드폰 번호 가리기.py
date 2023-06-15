31c862fc8ae006161a7c59f0b59fe0a06376d662
BaeYeonDoo <isabelle2k@naver.com>
2023-06-16 02:56:06 +0900

    Add 다른 방법 in 핸드폰 번호 가리기.py

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -2,3 +2,8 @@ def solution(phone_number):
     answer = ''
     for _ in range(len(phone_number)-4): answer += '*'
     return answer+phone_number[len(phone_number)-4:]
+
+'''
+def solution(phone_number):
+    return '*'*(len(phone_number)-4) + phone_number[len(phone_number)-4:]
+'''
