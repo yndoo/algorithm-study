2c04a36c2b37d675dd8c6ebe037e4a911b43badd
BaeYeonDoo <isabelle2k@naver.com>
2023-08-08 15:34:15 +0900

    [Gold IV] Title: N-Queen, Time: 27404 ms, Memory: 197116 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -2,26 +2,28 @@ import sys
 myinput = sys.stdin.readline
 
 N= int(myinput())
-col = [0] * (N+1)
+col = [0] * (N)
 answer = 0
 
-def isokay(i,col):
-    k = 1
+def isokay(i):
+    k = 0
     while k<i:
         if abs(col[i]-col[k])==abs(i-k) or (col[i]==col[k]):
             return False
         k+=1
     return True
 
-def finding(i, col):
+def finding(i):
     global answer
-    if isokay(i,col):
-        if i == N:
-            answer += 1
-        else:
-            for j in range(1,N+1):
-                col[i+1]=j
-                finding(i+1,col)
+    if i==N:
+        answer+=1
+    else:
+        for j in range(N):
+            col[i] = j
+            if isokay(i):
+                finding(i+1)
 
-finding(0, col)
+
+
+finding(0)
 print(answer)
