2bf04b0fa07e818127bb72459efeccdb20b20325
daisy <isabelle2k@naver.com>
2023-09-19 16:09:56 +0900

    [Gold V] Title: 탑, Time: 100 ms, Memory: 23688 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,22 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+int arr[5000001];
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	stack<int> s;
-	map<int, int> m;
-	int N;
-	cin >> N; //N 입력받기
+	int N, x;
+	
+	cin >> N;
+	
 	for (int i = 0; i < N; i++) { //N회 반복
-		int x;
 		cin >> x;				
-		m[x] = i + 1;				//N번째 탑의 높이 입력받으며 몇 번째 인덱스인지 map에 저장
-		while(!s.empty() && s.top() < x) { //s.top()에 나보다 큰 건물이 있을때까지 pop
+		arr[i] = x;				//N번째 탑의 높이 입력받으며 몇 번째 인덱스인지 map에 저장
+		while(!s.empty() && arr[s.top()] < x) { //s.top()에 나보다 큰 건물(의 인덱스)가 있을때까지 pop
 			s.pop();
 		}
-		if (!s.empty()) { cout << m[s.top()]; }	//스택이 비어있지 않다면 s.top()이 나의 레이저를 수신하는 탑
+		if (!s.empty()) { cout << s.top()+1; }	//스택이 비어있지 않다면 s.top()이 나의 레이저를 수신하는 탑의 인덱스
 		else { cout << 0; }						//스택이 비어있다면 레이저 수신하는 탑 없음 -> 0출력
-		s.push(x);
+		s.push(i);								//스택에 인덱스를 push
 		if (i != N - 1) { cout << " "; }		//출력형식 맞추기용 공백
 	}
 
