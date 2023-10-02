6cbb90e83d309d67559c221f70b35fb48e098da1
daisy <isabelle2k@naver.com>
2023-10-02 23:03:44 +0900

    [Gold IV] Title: 전화번호 목록, Time: 416 ms, Memory: 3016 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int t, n;
+string cur_numbers, tmpstr;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> t;
+	//test case t회
+	for (int turn = 0; turn < t; turn++) {
+		bool answer_flag = true;
+		map<string, int> m;		//전화번호부와 순서
+		string numArr[10000];	//전체 전화번호부
+		cin >> n;				//각 tc별 전화번호 수
+		//1. 전화번호가 1~9자리면 map에 등록
+		for (int nums = 0; nums < n; nums++) {
+			cin >> cur_numbers;
+			numArr[nums] = cur_numbers;
+			m[cur_numbers] = nums; //전화번호부에 등록
+		}
+
+		//2. 전체 전화번호 돌면서 map에 해당하는 곳이 있는지 확인
+		for (int i = 0; i < n; i++) {
+			tmpstr = numArr[i];
+			for (int tmpsize = 1; tmpsize < tmpstr.size()+1; tmpsize++) { //tmpstr의 문자열을 개수별로 검색
+				if (m.find(tmpstr.substr(0, tmpsize)) != m.end() && i!=m[tmpstr.substr(0, tmpsize)]) {
+					//존재하면 일관성이 없으므로 "NO"
+					answer_flag = false;
+				}
+			}
+		}
+		if (answer_flag) cout << "YES\n";
+		else cout << "NO\n";
+	}
+	return 0;
+}
