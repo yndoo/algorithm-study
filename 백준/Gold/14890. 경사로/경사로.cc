566561466a4cfb2dde832970aeaad30464df8e14
daisy <isabelle2k@naver.com>
2023-10-04 19:02:12 +0900

    Update 경사로.cc 주석

--- 백준/Gold/14890. 경사로/경사로.cc
+++ 백준/Gold/14890. 경사로/경사로.cc
@@ -110,3 +110,27 @@ int main() {
 	cout << answer;
 	return 0;
 }
+
+/*
+문제 : 가로 세로길을 지나갈 수 있는지? + 조건 만족하면 L길이의 경사로 놓고 지나갈 수 있음
+각 길을 한 칸씩 지나갈 수 있는지 세어서 그 개수가 N개가 되면 answer++;
+
+0.graph에 지도 입력받기
+
+1. 가로 방향 확인하는 for문
+1-1. 올라가는 경사로 방향 확인 → 두 계단사이 1만큼 증가한다면 isflat_row(row, start, end) 함수 돌리기 → 경사로를 둘 수 있으면 cnt_row++; stairs[][] = true;
+** isflat_row 함수에서는 start부터 end까지가 평평한지 확인하며, 이미 그곳에 경사로가 놓아졌는지도 확인해줌. 모두 경사로를 새로 둘수있으며, 전부 평평하면 true반환. 
+** stairs 배열은 경사로가 놓여지는 곳을 기록하는 배열.
+1-2. 내려가는 경사로 확인 → 두 계단사이 1만큼 감소한다면 isflat_row(row, start, end) 함수 돌리기→ 경사로를 둘 수 있으면 cnt_row++; stairs[][] = true;
+1-3. 경사로 없이 평평한 길 몇개있는지 확인, 있으면 cnt_row++;
+1-4. cnt_row가 N-1개라면 길건너기 가능. answer++;
+1-5. 세로 길 확인하기전에 이미 경사로가 있는지 확인하는 stairs배열을 초기화해야함.(가로와 세로 경사로가 따로이기 때문)
+
+2. 세로 방향 for문
+2-1. 올라가는 경사로  확인 → 두 계산사이 1증가하면 isflat_col(col, start, end) → 경사로 둘 수 있으면 cnt_col++; stairs[][] = true;
+2-2. 내려가는 경사로 → 두 계산사이 1감소하면 isflat_col(col, start, end) → 경사로 둘 수 있으면 cnt_col++; stairs[][] = true;
+2-3. 평평한 길 개수 cnt_col++;
+2-4. cnt_col == N-1 라면 answer++;
+
+3. cout<<answer;
+*/
