dde0550cdd3f20e554b24a7d46e0217f779627c4
daisy <isabelle2k@naver.com>
2023-10-09 19:00:42 +0900

    [Gold IV] Title: 좋은수열, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2661. 좋은수열/좋은수열.cc
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int N;
+string answer;
+bool flag = false;
+bool check(string numstr) { //인접한 동일 부분 수열이 있는지 체크하는 함수
+	//string numstr = to_string(num);
+	//cout << "numstr : " << numstr << "\n";
+	for (int i = 0; i < numstr.size(); i++) {
+		for (int j = 1; j < numstr.size() - i; j++) {
+			// numstr의 i번째에서 j개 자른 부분수열이 i+j 뒷쪽에 또 있으면 안 됨
+			if (numstr.substr(i+j, j).find(numstr.substr(i, j)) != string::npos) {
+				//cout << numstr.substr(i + j)<<" @\n";
+				//cout << numstr.substr(i, j)<<" &\n";
+				return false;
+			}
+			
+		}
+	}
+		return true;
+}
+void number(string num, int cnt) { //N 자릿수의 최소 좋은 수열을 만들어가는 함수
+	if (check(num) && !flag) {
+		if (cnt == N) {
+			flag = true;
+			answer = num; //최소 숫자가 가장 먼저 들어오기 때문에 flag로 한 번만 갱신
+			return;
+		}
+		else {
+			//마지막 숫자와 안 겹치는 가장 작은 수 넣어 재귀
+			for (int i = 1; i <= 3; i++) {
+				if (i != num[0]) number(num + to_string(i), cnt + 1);
+			}
+		}
+	}
+}
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> N;
+	number("1", 1);
+	if (flag) cout << answer; return 0;
+	number("2", 1);
+	number("3", 1);
+	cout << answer;
+	return 0;
+}
