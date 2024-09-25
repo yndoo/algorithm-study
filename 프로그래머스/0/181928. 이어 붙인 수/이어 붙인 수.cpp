c335de6cd4e631995d2e085aa877e38b91eef559
yndoo <isabelle2k@naver.com>
2024-09-26 02:41:27 +0900

    Update 이어 붙인 수.cpp

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
@@ -5,20 +5,44 @@ using namespace std;
 
 int solution(vector<int> num_list) {
     int answer = 0;
-    vector<int> even;
-    vector<int> odd;
-    for(int num : num_list)
+    int even = 0;
+    int odd = 0;
+    for(int i = num_list.size() - 1; i >= 0; --i)
     {
-        if(num % 2 == 0) even.push_back(num);
-        else odd.push_back(num);
+        if(num_list[i] % 2 == 0)
+        {
+            answer += num_list[i] * pow(10, even++);
+        }
+        else
+        {
+            answer += num_list[i] * pow(10, odd++);
+        }
     }
-    for(int i = 0; i<even.size(); ++i)
-    {
-        answer += even[i] * pow(10, even.size() - i - 1);
-    }
-    for(int i = 0; i<odd.size(); ++i)
-    {
-        answer += odd[i] * pow(10, odd.size() - i - 1);
-    }
+
     return answer;
 }
+
+//#include <string>
+//#include <vector>
+//#include <cmath>
+//using namespace std;
+//
+//int solution(vector<int> num_list) {
+//    int answer = 0;
+//    vector<int> even;
+//    vector<int> odd;
+//    for (int num : num_list)
+//    {
+//        if (num % 2 == 0) even.push_back(num);
+//        else odd.push_back(num);
+//    }
+//    for (int i = 0; i < even.size(); ++i)
+//    {
+//        answer += even[i] * pow(10, even.size() - i - 1);
+//    }
+//    for (int i = 0; i < odd.size(); ++i)
+//    {
+//        answer += odd[i] * pow(10, odd.size() - i - 1);
+//    }
+//    return answer;
+//}
